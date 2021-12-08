bd886bb42a32fa76597752accd130eb58e3a558f
Rodrigo Nigri Griner <rodrigong1@al.insper.edu.br>
2021-12-07 22:43:30 -0300

    Heroku

--- recepie/settings.py
+++ recepie/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c5ege9e*6yn5*)!+k$tsjexlz)wv7uy)levweha21^-dp-5$lc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['serene-sands-20198.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -78,10 +79,11 @@ WSGI_APPLICATION = 'recepie.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='postgresql://localhost/recepie?user=recepieuser&password=recepiesenha',
+        conn_max_age=600,
+        ssl_require=not DEBUG
+    )
 }
 
 
